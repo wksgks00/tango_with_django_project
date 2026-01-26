ee3b4b3c1c2e4325453b2070cb5e54b5a86e952c
Alina Palamarchuk <3033663p@student.gla.ac.uk>
2026-01-26 03:52:54 +0000

    Exercise for 3.5 changed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-    path('about/', views.about, name='about'),
 
 ]
 
