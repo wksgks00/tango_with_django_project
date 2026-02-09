f778ad0c7ad7f12fe8f51357b7f04ee98b708d67
Alina Palamarchuk <3033663p@student.gla.ac.uk>
2026-02-09 22:08:45 +0000

    Chapter 9 + exercises complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
 
@@ -14,3 +14,4 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -132,3 +132,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+LOGIN_URL = 'rango:login'
